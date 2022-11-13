358e420930f98cfe6ffd44c717ed17fba95109e5
cros6y6raga <crosbybraga@gmail.com>
2022-11-13 09:56:32 +0300

    I added a local state and prototyped

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+    const tasks1 = [
+        {id: 1, title: "Training", isDone: true},
+        {id: 2, title: "Food", isDone: true},
+        {id: 3, title: "Film", isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: "Anime", isDone: true},
+        {id: 2, title: "UFC", isDone: false},
+        {id: 3, title: "Beer", isDone: false}
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={'Monday'} tasks={tasks1}/>
+            <Todolist title={'Tuesday'} tasks={tasks2}/>
         </div>
     );
 }
